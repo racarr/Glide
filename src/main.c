38f07c2fa06192ca679f9cd45d1bd1f73a462e0d
Robert Carr <racarr@gnome.org>
2010-01-29 13:33:46 -0500

    Make a window

--- src/main.c
+++ src/main.c
@@ -27,13 +27,59 @@
 
 #include <gtk/gtk.h>
 
+#include <clutter/clutter.h>
+#include <clutter-gtk/clutter-gtk.h>
+
 
 #include <glib/gi18n.h>
+
+static void
+glide_setup_main_window (GtkBuilder *builder)
+{
+	ClutterActor *stage;
+	ClutterColor black = {0x00, 0x00, 0x00, 0xff};
+	GtkWidget *embed, *window, *vbox;
+
+	window = GTK_WIDGET (gtk_builder_get_object (builder, "window1"));
+	gtk_window_set_default_size (GTK_WINDOW (window), 600, 500);
+
+	vbox = GTK_WIDGET (gtk_builder_get_object (builder, "vbox1"));
+	
+	embed = gtk_clutter_embed_new ();
+	gtk_container_add (GTK_CONTAINER (vbox), embed);
+	
+
+	stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (embed));
+	clutter_actor_set_size (stage, 500, 500);
+	
+	gtk_widget_set_size_request (embed, 500, 500);
+	
+	clutter_stage_set_color (CLUTTER_STAGE (stage), &black);	
+	clutter_actor_show (stage);
+
+	gtk_widget_show_all (window);
+}
 
 int
 main (int argc, char *argv[])
 {
- 	GtkWidget *window;
+	GError *e = NULL;
+	GtkBuilder *builder;
+
+	gtk_set_locale ();
+	gtk_init (&argc, &argv);
+	gtk_clutter_init (&argc, &argv);
+
+	builder = gtk_builder_new ();
+	gtk_builder_add_from_file (builder, "main.ui",
+							   &e);
+	if (e)
+		{
+			g_critical ("Failed to load main UI: %s", e->message);
+			g_error_free (e);
+
+			return 0;
+		}
 
 #ifdef ENABLE_NLS
 	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
@@ -41,11 +87,7 @@ main (int argc, char *argv[])
 	textdomain (GETTEXT_PACKAGE);
 #endif
 
-	gtk_set_locale ();
-	gtk_init (&argc, &argv);
-
-	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_widget_show (window);
+	glide_setup_main_window (builder);
 
 	gtk_main ();
 	return 0;
