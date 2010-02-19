1ade326c94a95e8a519867df0a60e74d913fb0fc
Robert Carr <racarr@gnome.org>
2010-02-18 23:55:50 -0500

    Missing files?

---
+++ src/glide-manipulator-priv.h
@@ -0,0 +1,67 @@
+/*
+ * glide-manipulator-private.h
+ * This file is part of glide
+ *
+ * Copyright (C) 2010 - Robert Carr
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANMANIPULATORILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+#ifndef __GLIDE_MANIPULATOR_PRIVATE_H__
+#define __GLIDE_MANIPULATOR_PRIVATE_H__
+
+#include "glide-manipulator.h"
+
+G_BEGIN_DECLS
+
+typedef enum
+{
+  WIDGET_NONE,
+  WIDGET_TOP_LEFT,
+  WIDGET_TOP_RIGHT,
+  WIDGET_BOTTOM_LEFT,
+  WIDGET_BOTTOM_RIGHT
+} GlideManipulatorWidget;
+
+typedef enum
+  {
+    WIDGET_MODE_RESIZE,
+    WIDGET_MODE_ROTATE
+  } GlideManipulatorWidgetMode;
+
+struct _GlideManipulatorPrivate
+{
+  ClutterActor *target;
+  
+  GlideManipulatorWidgetMode mode;
+
+  gboolean dragging;
+  gfloat drag_center_x;
+  gfloat drag_center_y;
+  
+  GlideManipulatorWidget hovered;
+  
+  gboolean transforming;
+  GlideManipulatorWidget resize_widget;
+
+  gboolean swap_widgets;
+  
+  gfloat rot_angle;
+};
+
+G_END_DECLS
+
+#endif  /* __GLIDE_MANIPULATOR_PRIVATE_H__  */
---
+++ src/glide-window-private.h
@@ -0,0 +1,37 @@
+/*
+ * glide-window-private.h
+ * This file is part of glide
+ *
+ * Copyright (C) 2010 - Robert Carr
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANWINDOWILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+#ifndef __GLIDE_WINDOW_PRIVATE_H__
+#define __GLIDE_WINDOW_PRIVATE_H__
+
+#include "glide-window.h"
+
+G_BEGIN_DECLS
+
+struct _GlideWindowPrivate
+{
+  ClutterActor *stage;
+};
+
+G_END_DECLS
+
+#endif  /* __GLIDE_WINDOW_PRIVATE_H__  */
