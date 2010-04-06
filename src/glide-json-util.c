9944c0a9f40a87fc0cb9e0caff0875ae17672828
Robert Carr <racarr@gnome.org>
2010-04-06 16:50:19 -0400

    Add some JSON utilities for implementing actor serialize

---
+++ src/glide-json-util.c
@@ -0,0 +1,53 @@
+/*
+ * glide-json-util.c
+ * This file is part of glide
+ *
+ * Copyright (C) 2010 - Robert Carr
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "glide-json-util.h"
+
+void
+glide_json_object_set_string (JsonObject *obj, const gchar *prop, const gchar *value)
+{
+  JsonNode *n = json_node_new (JSON_NODE_VALUE);
+  
+  json_node_set_string (n, value);
+  json_object_set_member (obj, prop, n);
+}
+
+void
+glide_json_object_set_double (JsonObject *obj, const gchar *prop, gdouble value)
+{
+  JsonNode *n = json_node_new (JSON_NODE_VALUE);
+  
+  json_node_set_double (n, value);
+  json_object_set_member (obj, prop, n);
+}
+
+void
+glide_json_object_add_actor_geometry (JsonObject *obj, ClutterActor *actor)
+{
+  ClutterGeometry geom;
+  
+  clutter_actor_get_allocation_geometry (actor, &geom);
+  glide_json_object_set_double (obj, "x", geom.x);
+  glide_json_object_set_double (obj, "y", geom.y);
+  glide_json_object_set_double (obj, "width", geom.width);
+  glide_json_object_set_double (obj, "height", geom.height);
+}
